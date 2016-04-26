97a92126ea1d44974ebeb58b79bcde2f2050ed89
Wladimir Bandeira <Wladimir Bandeira>
2016-04-26 07:55:38 -0300

    Esse projeto que está no repositório com finalidade de estudo

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,44 @@
+package br.com.casadocodigo.loja.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+@RequestMapping("produtos")
+public class ProdutosController {
+
+	@Autowired
+	private ProdutoDAO produtoDao;
+
+	@RequestMapping("/form")
+	public ModelAndView form() {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		return modelAndView;
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public String gravar(Produto produto) {
+		produtoDao.gravar(produto);
+		System.out.println(produto);
+		return "produtos/ok";
+	}
+	
+	@RequestMapping( method=RequestMethod.GET)
+	public ModelAndView listar(){
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
+	}
+
+}
